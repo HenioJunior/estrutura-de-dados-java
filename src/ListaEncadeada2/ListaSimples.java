2500db0f90ef40c29ea53d4ed2c290d0d19f324c
Henio Junior <henioajunior@gmail.com>
2021-03-25 19:35:09 -0300

    Lista Encadeada 2

--- src/ListaEncadeada2/ListaSimples.java
+++ src/ListaEncadeada2/ListaSimples.java
@@ -47,9 +47,62 @@ public class ListaSimples {
 
     }//----------------------------------------Inserir Primeiro
 
+    public boolean removerNode (String nome) {
+        Node atual = this.primeiro;
+        Node anterior = null;
+        if(eVazia()) {
+            return false;
+        }
+        else {
+            while((atual != null) && (!atual.getP().getNome().equals(nome))) {
+                anterior = atual;
+                atual = atual.getProximo();
+            }
+            if(atual == this.primeiro) {
+                if(this.primeiro == this.ultimo) {
+                    this.ultimo = null;
+                }
+                this.primeiro = this.primeiro.getProximo();
+            }
+            else {
+                if(atual == this.ultimo) {
+                    this.ultimo = anterior;
+                }
+                anterior.setProximo(atual.getProximo());
+            }
+            this.quantNode--;
+            return true;
+        }
+
+    }//-----------------------------------------------removerNode
+
+    public String pesquisarNode (String nome) {
+        String msg ="";
+        Node atual = this.primeiro;
+        while((atual != null) && (!atual.getP().getNome().equals(nome))) {
+            atual = atual.getProximo();
+        }
+        return msg = "Nome: " + atual.getP().getNome() + "\n" +
+                     "Sexo: " + atual.getP().getSexo() + "\n" +
+                     "Idade: " + atual.getP().getIdade();
+    }//----------------------------------------------pesquisarNode
+
+    public String imprimirLista() {
+        String msg = "";
+        if(eVazia()) {
+            msg = "A lista está vazia!";
+        }
+        else {
+            Node atual = this.primeiro;
+            while( atual != null) {
+                msg += atual.getP().getNome() + " -> ";
+                atual = atual.getProximo();
+            }
+        }
+        return msg;
+    }//----------------------------------------------imprimirLista
+
     public boolean eVazia() {
         return (this.primeiro == null);
-    }//--------------------------------------------------eVazia
-
-
+    }//-----------------------------------------------------eVazia
 }
