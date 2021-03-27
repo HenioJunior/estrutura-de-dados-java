13e0ec103db20c69c7432d9394e482fa3f138da8
Henio Junior <henioajunior@gmail.com>
2021-03-27 10:28:54 -0300

    Busca Binaria

--- src/BuscaSequencialEBinaria/Colecao.java
+++ src/BuscaSequencialEBinaria/Colecao.java
@@ -1,7 +1,8 @@
 package BuscaSequencialEBinaria;
 
 public class Colecao {
-    private int valores[] = {12,56,87,23,96,43,27,76,33,11};
+    //private int valores[] = {12,56,87,23,96,43,27,76,33,11};
+    private int valores[] = {11,12,23,27,33,43,56,76,87,96};
 
     public int buscaSequencial(int val) {
         int pos;
@@ -11,5 +12,39 @@ public class Colecao {
             }
         }
         return -1;
-    }
+    }//----------------------------------------------------------------------------buscaSequencial
+
+    public int buscaBinaria(int val) {
+        int meio, inicio, fim;
+        inicio = 0;
+        fim = valores.length-1;
+        while(inicio <= fim) {
+            meio = (inicio + fim) / 2;
+            System.out.println("DEBUG - I: "+inicio+" F: "+fim+" M: "+meio);
+            if(val == valores[meio]) {
+              return meio;
+            }
+            else {
+                if(val > valores[meio]) {
+                    inicio = meio + 1;
+                }
+                else {
+                    fim = meio - 1;
+                }
+            }
+        }
+        return -1;
+    }//-------------------------------------------------------------------------------buscaBinaria
 }
+
+//Teste
+//{11,12,23,27,33,43,56,76,87,96};
+//  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+//Numero escolhido? 99
+//inicio      fim         meio
+//     0        9            4
+//     5        9            7
+//     8        9            8
+//     9        9            9
+//     10       9
--- src/BuscaSequencialEBinaria/MyApp.java
+++ src/BuscaSequencialEBinaria/MyApp.java
@@ -3,8 +3,15 @@ package BuscaSequencialEBinaria;
 public class MyApp {
     public static void main(String[] args) {
         Colecao colecao = new Colecao();
+        System.out.println("-------------------Busca Sequencial------------------------");
         System.out.println("Posição do elemento 23 = " + colecao.buscaSequencial(23));
         System.out.println("Posição do elemento 12 = " + colecao.buscaSequencial(12));
+        System.out.println("Posição do elemento 76 = " + colecao.buscaSequencial(76));
+        System.out.println("Posição do elemento 100 = " + colecao.buscaSequencial(100));
+        System.out.println("\n\n");
+        System.out.println("-------------------Busca Binária----------------------------");
+        System.out.println("Posição do elemento 23 = " + colecao.buscaBinaria(23));
+        System.out.println("Posição do elemento 12 = " + colecao.buscaBinaria(12));
         System.out.println("Posição do elemento 76 = " + colecao.buscaSequencial(76));
         System.out.println("Posição do elemento 100 = " + colecao.buscaSequencial(100));
     }
