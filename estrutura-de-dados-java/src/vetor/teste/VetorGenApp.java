d7aa107fdab498125d396f0807248b0c10b0e12e
Henio Junior <henioajunior@gmail.com>
2021-04-07 17:25:02 -0300

    Vetores - Definindo o tipo de vetor dinamicamente

---
+++ estrutura-de-dados-java/src/vetor/teste/VetorGenApp.java
@@ -0,0 +1,36 @@
+package vetor.teste;
+
+import vetor.VetorGenerico;
+
+public class VetorGenApp {
+
+    public static void main(String[] args) {
+
+        VetorGenerico<Contato> vetor = new VetorGenerico(10);
+
+        Contato c1 = new Contato("Contato1", "1234567", "contato1@email.com");
+        Contato c2 = new Contato("Contato2", "2234567", "contato2@email.com");
+        Contato c3 = new Contato("Contato3", "3234567", "contato3@email.com");
+
+        Contato c4 = new Contato("Contato1", "1234567", "contato1@email.com");
+
+        vetor.adiciona(c1);
+        vetor.adiciona(c2);
+        vetor.adiciona(c3);
+
+        System.out.println(vetor);
+        System.out.println("-------------------------");
+
+        System.out.println("Tamanho = " + vetor.tamanho());
+        System.out.println("-------------------------");
+
+        int pos = vetor.busca(c4);
+        if (pos > -1) {
+            System.out.println("Elemento existe no vetor");
+        } else {
+            System.out.println("Elemento ñ existe no vetor");
+        }
+        System.out.println("-------------------------");
+
+    }
+}
